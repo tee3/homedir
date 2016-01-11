69947d8ef065adba1c82d9846813a13891091233
Thomas Brown <tabsoftwareconsulting@gmail.com>
2016-01-11 13:04:30 -0500

    [cmd] Use "rem" instead of "REM".
    It's just too loud.

--- dotfiles/.cmd.bat
+++ dotfiles/.cmd.bat
@@ -1,9 +1,10 @@
 @echo off
-REM A tool to customize a cmd.exe shell
+rem
+rem A tool to customize a cmd.exe shell
 
-REM Set HOME if it is not set.
+rem Set HOME if it is not set.
 if "%HOME%" == "" set HOME=C:\Users\%USERNAME%
 
-REM Run a new shell with HOME set and a chance to set up the
-REM environment.
+rem Run a new shell with HOME set and a chance to set up the
+rem environment.
 "%HOME%"\.cmdrc.bat
