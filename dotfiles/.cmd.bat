d7205ea0b56d51349b88603fb87ead0b74984f0b
Thomas Brown <tabsoftwareconsulting@gmail.com>
2016-01-11 11:05:37 -0500

    [cmd] Conditionally set HOME.
    This allows the user to set HOME differently than homedir does by
    default.

--- dotfiles/.cmd.bat
+++ dotfiles/.cmd.bat
@@ -1,7 +1,9 @@
 @echo off
 REM A tool to customize a cmd.exe shell
 
+REM Set HOME if it is not set.
+if "%HOME%" == "" set HOME=C:\Users\%USERNAME%
+
 REM Run a new shell with HOME set and a chance to set up the
 REM environment.
-set HOME=C:\Users\%USERNAME%
 "%HOME%"\.cmdrc.bat
