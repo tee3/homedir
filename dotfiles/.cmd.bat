7da8268850d4478ddd93cf7fa85e2450cf5359a9
Thomas Brown <tabsoftwareconsulting@gmail.com>
2016-07-04 08:05:55 -0700

    [cmd] Ensure that the HOME directory exists.
    This is to protect against different version of Windows will differing
    locations for user files.

--- dotfiles/.cmd.bat
+++ dotfiles/.cmd.bat
@@ -4,6 +4,7 @@ rem A tool to customize a cmd.exe shell
 
 rem Set HOME if it is not set.
 if "%HOME%" == "" set HOME=C:\Users\%USERNAME%
+if not exist "%HOME%" echo error: HOME, "%HOME%", does not exist && exit /b 1
 
 rem Run a new shell with HOME set and a chance to set up the
 rem environment.
