ff37368f8cf4411f65dd8d1209045b4dc4d1416f
Thomas Brown <tabsoftwareconsulting@gmail.com>
2017-04-29 19:57:33 -0400

    [cmd] Check that programs are actually installed.

--- dotfiles/.cmdrc.bat
+++ dotfiles/.cmdrc.bat
@@ -8,6 +8,7 @@ if exist "%CMDOPTLOCAL%" set PATH=%CMDOPTLOCAL%\bin;%PATH%
 
 rem Git
 set CMDGITROOT=C:\Git
+if not exist "%CMDGITROOT%" echo Git is not installed at "%CMDGITROOT%".
 if exist "%CMDGITROOT%" set PATH=%CMDGITROOT%\usr\bin;%PATH%
 
 rem SSH
@@ -15,27 +16,32 @@ call start-ssh-agent
 
 rem Python
 set CMDPYTHONROOT=C:\Python27
+if not exist "%CMDPYTHONROOT%" echo Python is not installed at "%CMDPYTHONROOT%".
 if exist "%CMDPYTHONROOT%" set PATH=%CMDPYTHONROOT%;%CMDPYTHONROOT%\Scripts;%PATH%
 rem @todo would be better if this used same approach as bash
 if exist "%CMDPYTHONROOT%" set PATH=%HOME%\AppData\Roaming\Python\Scripts;%PATH%
 
 rem Ruby
 set CMDRUBYROOT=C:\Ruby23
+if not exist "%CMDRUBYROOT%" echo Ruby is not installed at "%CMDRUBYROOT%".
 rem @todo would be better if this used same approach as bash
 if exist "%CMDRUBYROOT%" set PATH=%HOME%\.gem\ruby\2.3.0\bin;%PATH%
 
 rem Node.js
 set CMDNODEJSROOT=C:\nodejs
+if not exist "%CMDNODEJSROOT%" echo NodeJS is not installed at "%CMDNODEJSROOT%".
 set CMDNPMPREFIX=%HOME%\.node_modules
 if exist "%CMDNODEJSROOT%" set PATH=%CMDNPMPREFIX%\bin;%PATH%
 if exist "%CMDNODEJSROOT%" set NODE_PATH=%CMDNPMPREFIX%\lib\node_modules:%NODE_PATH%
 
 rem Go
 set CMDGOROOT=C:\Go
+if not exist "%CMDGOROOT%" echo Go is not installed at "%CMDGOROOT%".
 if exist "%CMDGOROOT%" set PATH=%CMDOPTLOCAL%\src\go\bin;%PATH%
 
 rem Emacs
 set CMDEMACSROOT=C:\Emacs
+if not exist "%CMDEMACSROOT%" echo Emacs is not installed at "%CMDEMACSROOT%".
 if exist "%CMDEMACSROOT%" set PATH=%CMDEMACSROOT%\bin;%PATH%
 if exist "%CMDEMACSROOT%" (
    set ALTERNATE_EDITOR=
