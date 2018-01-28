e58bbc64291e0241793b39d0939cecfb27eb1cf5
Thomas Brown <tabsoftwareconsulting@gmail.com>
2018-01-28 04:18:45 -0500

    [cmd] Ensure that the working directory is HOME.

--- dotfiles/.cmd.bat
+++ dotfiles/.cmd.bat
@@ -6,6 +6,8 @@ rem Set HOME if it is not set.
 if "%HOME%" == "" set HOME=C:\Users\%USERNAME%
 if not exist "%HOME%" echo error: HOME, "%HOME%", does not exist && exit /b 1
 
+cd "%HOME%"
+
 rem Run a new shell with HOME set and a chance to set up the
 rem environment.
 "%HOME%"\.cmdrc.bat
