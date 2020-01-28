518103257369b726f686785388d54e4372fcf276
Thomas Brown <tabsoftwareconsulting@gmail.com>
2020-01-28 03:57:35 -0500

    WIP [emacs] A Emacs configuration using only standard packages.

---
+++ dotfiles/.emacs.vanilla.el
@@ -0,0 +1,13 @@
+;;; .emacs.vanilla --- Emacs user configuration file using standard Emacs packages.
+;;;
+;;; URL: https://tee3.github.com/homedir
+;;;
+;;; Commentary:
+;;;
+;;;    This file customizes Emacs for a specific user's needs.
+;;;
+;;; Code:
+
+
+(provide '.emacs.vanilla)
+;;; .emacs ends here
