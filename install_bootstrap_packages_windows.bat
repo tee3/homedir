6d73087e40da5081f19e52d88539e682edc7fc4f
Thomas Brown <tabsoftwareconsulting@gmail.com>
2022-08-23 09:18:23 -0400

    [bootstrap] Install Microsoft Visual Studio Build Tools on Windows.
    This installs the command line tools as opposed to Microsoft Visual
    Studio, which does not install the compilers by default.

--- install_bootstrap_packages_windows.bat
+++ install_bootstrap_packages_windows.bat
@@ -14,8 +14,8 @@ set /p id="Press Enter when complete ..."
 echo "Install the Windows Package Manager CLI (`winget`) from https://github.com/microsoft/winget-cli."
 set /p id="Press Enter when complete ..."
 
-winget install --silent --exact --id Microsoft.VisualStudio.2022.Community --accept-package-agreements --accept-source-agreements
-if %ERRORLEVEL% NEQ 0 echo "error: failed to install Microsoft.VisualStudio.2022.Community."
+winget install --silent --exact --id Microsoft.VisualStudio.2022.BuildTools --accept-package-agreements --accept-source-agreements
+if %ERRORLEVEL% NEQ 0 echo "error: failed to install Microsoft.VisualStudio.2022.BuildTools."
 winget install --silent --exact --id Python.Python.3 --accept-package-agreements --accept-source-agreements
 if %ERRORLEVEL% NEQ 0 echo "error: failed to install Python.Python."
 winget install --silent --exact --id Git.Git --accept-package-agreements --accept-source-agreements
