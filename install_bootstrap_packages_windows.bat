b448df65d4901d9c2cda6c533c6ef2b335fbb5c9
Thomas Brown <tabsoftwareconsulting@gmail.com>
2022-08-23 11:15:08 -0400

    [bootstrap] Accept package/source agreements on Windows.

--- install_bootstrap_packages_windows.bat
+++ install_bootstrap_packages_windows.bat
@@ -14,9 +14,9 @@ set /p id="Press Enter when complete ..."
 echo "Install the Windows Package Manager CLI (`winget`) from https://github.com/microsoft/winget-cli."
 set /p id="Press Enter when complete ..."
 
-winget install --silent --exact --id Microsoft.VisualStudio.2019.Community
+winget install --silent --exact --id Microsoft.VisualStudio.2019.Community --accept-package-agreements --accept-source-agreements
 if %ERRORLEVEL% NEQ 0 echo "error: failed to install Microsoft.VisualStudio.2019.Community."
-winget install --silent --exact --id Python.Python.3
+winget install --silent --exact --id Python.Python.3 --accept-package-agreements --accept-source-agreements
 if %ERRORLEVEL% NEQ 0 echo "error: failed to install Python.Python."
-winget install --silent --exact --id Git.Git
+winget install --silent --exact --id Git.Git --accept-package-agreements --accept-source-agreements
 if %ERRORLEVEL% NEQ 0 echo "error: failed to install Git.Git."
